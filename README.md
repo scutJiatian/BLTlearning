d7994cdb6ef9aa8a11bc49f18b05617c7a10c94e
scutJiatian <641484513@qq.com>
2021-03-15 16:17:40 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# BLTlearning
-git flow控制流以及echarts学习项目
+# echarts_learning
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+  
+    <Echarts />
+  </div>
+</template>
+
+<script>
+
+import Echarts from './components/echarts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Echarts
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
